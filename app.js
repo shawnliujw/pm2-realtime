1a844d523953e245c48fc2e943d6486bcc0d6e5d
Shawn Liu <shawn.liu@dotter.me>
2014-12-19 14:38:51 +0800

    test log output

--- app.js
+++ app.js
@@ -24,5 +24,18 @@ server.listen(config.port, function () {
     console.log('Express server listening on %d, in %s mode', config.port, app.get('env'));
 });
 
+var pm2 = require("./lib/PM2");
+pm2.listProcesses()
+    .then(function (info) {
+        logger.info(info);
+        pm2.monitor(3, function (d) {
+            console.log(d);
+        });
+    })
+    .catch(function (err) {
+        logger.error(err);
+        pm2.disconnect();
+    })
+
 // Expose app
 exports = module.exports = app;
