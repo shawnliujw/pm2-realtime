e950d8eecbe62e7a41a88a2a2c0d3856dbab8a3f
Shawn Liu <shawn.liu@dotter.me>
2014-12-19 14:34:32 +0800

    add log monitor

--- test/lib/PM2.test.js
+++ test/lib/PM2.test.js
@@ -6,7 +6,9 @@ var logger = require("log4js").getLogger("test/lib/PM2.test.js");
 pm2.listProcesses()
     .then(function (info) {
         logger.info(info);
-        pm2.monitor();
+        pm2.monitor(3, function (d) {
+            console.log(d);
+        });
     })
     .catch(function (err) {
         logger.error(err);
